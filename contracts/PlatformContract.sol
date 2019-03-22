3a8b8f6106ad98d15f62b38e9ff6f714209c5df0
Wei_da <da.11@osu.edu>
2019-03-22 16:51:53 -0400

    Version 0.1: basic function

--- contracts/PlatformContract.sol
+++ contracts/PlatformContract.sol
@@ -2,16 +2,22 @@ pragma solidity >=0.4.22 <0.6.0;
 
 contract VotingContract {
 
-    
+    /*    Normal user of the plantform:
+          Items: the */
     struct User {
+        address relatedAddress;
         mapping (Item => uint) items; 
         uint8 money;
+        bool authority;
     }
     
     struct Item {
+        address relatedAddress;
         uint8 relatedGameId; // 
         uint price;
         uint selledCount;
+        bool repeatable;
+        bool tradeable;
     }
 
     struct publisher {
@@ -36,14 +42,30 @@ contract VotingContract {
     }
 
     function regeister() {
+        require(msg.sender != administrator);
+        users[msg.sender] = User(msg.sender, null, 0, true);
+    }
 
+    function addMoney (uint8 count) {
+        require(msg.sender != administrator);
+        users[msg.sender].money = users[msg.sender].money + count;
     }
 
     function buyItem() {
 
     }
 
-    function queryItem() {
+    function tradeItem() {
 
-    } 
+    }
+
+    function banPerson(address userAddress) {
+        require(msg.sender == administrator);
+        users[userAddress].authority = false;
+    }
+
+    function unbanPerson() {
+        require(msg.sender == administrator);
+        users[userAddress].authority = true;;
+    }
 }

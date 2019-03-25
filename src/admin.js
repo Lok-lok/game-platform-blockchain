93abf515eea85af3d9c2c05e1f7c71b30d7aeab3
Lok <huang.2870@osu.edu>
2019-03-25 03:39:35 -0400

    Implement admin page

--- src/admin.js
+++ src/admin.js
@@ -64,124 +64,25 @@ App = {
     
     // Render Account
     $('#accountAddress').html(App.account)
-
-    await App.renderVoteResult()
-    await App.renderVote()
-    await App.renderOpen()
-    await App.renderHistory()
     
     App.setLoading(false)
   },
-
-  renderVoteResult: async () => {
-    const proposalCount = await App.voting.proposalCount()
-    const winner = await App.voting.getWin()
-    for (var i = 0; i < proposalCount; i++) {
-      const proposal = await App.voting.proposals(i)
-      $("#candidatesResults").append("<tr><th>" + i + "</th><td>" + proposal + "</td></tr>")
-    }
-    
-    if (winner.length != proposalCount) {
-      string = "Winner(s):"
-      for (var i = 0; i < winner.length; i++)
-        string += " " + winner[i].c[0]
-      $('#winner').html(string)
-    } else {
-      $('#winner').html("No winner determined previously")
-    }
-  },
-  
-  renderVote: async () => {
-    const openToVote = await App.voting.openToVote()
-    const proposalCount = await App.voting.proposalCount()
-    const vote_ret = await App.voting.getVote()
-    const vote = vote_ret.c[0]
-    const normal = vote_ret.s == 1
-    
-    const voter_div = $('#voter')
-    const vote_div = $('#vote')
-    const unvote_div = $('#unvote')
-    
-    if (openToVote) {
-      if (vote >= proposalCount) {
-        for (var i = 0; i < proposalCount; i++) {
-          $('#candidatesSelect').append("<option value='" + i + "' >" + i + "</ option>")
-        }
-        vote_div.show()
-        unvote_div.hide()
-      } else {
-        const status = $('#normal_ban')
-        $('#voteFor').html("Voted for: " + vote)
-        if (normal) {
-          status.html("Status: Normal")
-        } else {
-          status.html("Status: Banned")
-        }
-        vote_div.hide()
-        unvote_div.show()
-      }
-    } else {
-      voter_div.hide()
-    }
-  },
-
-  renderOpen: async () => {
-    const openToVote = await App.voting.openToVote()
-    
-    const startVoting_div = $('#startVoting')
-    const endVoting_div = $('#endVoting')
-    const chairperson_div = $('#chairperson')
-    const status = $('#status')
-    if (openToVote) {
-      status.html("OPEN")
-    } else {
-      status.html("CLOSED")
-    }
-    App.voting.chairperson().then(function(chairperson) {
-      if (App.account == chairperson) {
-        if (openToVote) {
-          startVoting_div.hide()
-          endVoting_div.show()
-        } else {
-          startVoting_div.show()
-          endVoting_div.hide()
-        }
-        chairperson_div.show()
-      } else {
-        chairperson_div.hide()
-      }
-    })
-  },
-  
-  renderHistory: async () => {
-    let logVote = App.voting.LogVote({}, {fromBlock: 0, toBlock: 'latest'})
-    logVote.get((error, logs) => {
-      // we have the logs, now print them
-      logs.forEach(log => {
-        let vote = log.args["vote"].s == 1
-        string = "<tr><th>" + log.args["vote"].c[0] + "</th><td>"
-        if (vote) {
-          string += "Vote"
-        } else {
-          string += "Unvote"
-        }
-        string += "</td><td>" + log.args["voter"] + "</td><td>" +  log.args["timestamp"].c[0] + "</td></tr>"
-        $("#history").append(string)
-      })
-    })
-  },
   
   
-  banUser: async () => {
+  banUser: async (string) => {
+    App.Platform.banUser(string)
   },
 
-  unBanUser: async () => {
+  unbanUser: async (string) => {
+    App.Platform.unbanUser(string)
   },
 
-  banPublisher: async () => {
+  banPublisher: async (string) => {
+    App.Platform.banPublisher(string)
   },
 
-  unBanPublisher: async () => {
+  unbanPublisher: async (string) => {
+    App.Platform.unbanPublisher(string)
   },
 
   

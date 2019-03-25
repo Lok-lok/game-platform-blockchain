bac1b385a149c1fee7ecc1e9decd206998812ef1
peudy14 <46737106+peudy14@users.noreply.github.com>
2019-03-25 11:34:02 -0400

    Add files via upload

--- src/user.js
+++ src/user.js
@@ -1,104 +1,109 @@
 App = {
-  loading: false,
-  contracts: {},
+    loading: false,
+    contracts: {},
 
-  load: async () => {
-    await App.loadWeb3()
-    await App.loadAccount()
-    await App.loadContract()
-    await App.render()
-  },
+    load: async() => {
+        await App.loadWeb3()
+        await App.loadAccount()
+        await App.loadContract()
+        await App.render()
+    },
 
-  loadWeb3: async () => {
-    if (typeof web3 !== 'undefined') {
-      App.web3Provider = web3.currentProvider
-      web3 = new Web3(web3.currentProvider)
-    } else {
-      window.alert("Please connect to Metamask.")
-    }
-    // Modern dapp browsers...
-    if (window.ethereum) {
-      window.web3 = new Web3(ethereum)
-      try {
-        // Request account access if needed
-        await ethereum.enable()
-        // Acccounts now exposed
-        web3.eth.sendTransaction({/* ... */})
-      } catch (error) {
-        // User denied account access...
-      }
-    }
-    // Legacy dapp browsers...
-    else if (window.web3) {
-      App.web3Provider = web3.currentProvider
-      window.web3 = new Web3(web3.currentProvider)
-      // Acccounts always exposed
-      web3.eth.sendTransaction({/* ... */})
-    }
-    // Non-dapp browsers...
-    else {
-      console.log('Non-Ethereum browser detected.')
-    }
-  },
+    loadWeb3: async() => {
+        if (typeof web3 !== 'undefined') {
+            App.web3Provider = web3.currentProvider
+            web3 = new Web3(web3.currentProvider)
+        } else {
+            window.alert("Please connect to Metamask.")
+        }
+        // Modern dapp browsers...
+        if (window.ethereum) {
+            window.web3 = new Web3(ethereum)
+            try {
+                // Request account access if needed
+                await ethereum.enable()
+                    // Acccounts now exposed
+                web3.eth.sendTransaction({ /* ... */ })
+            } catch (error) {
+                // User denied account access...
+            }
+        }
+        // Legacy dapp browsers...
+        else if (window.web3) {
+            App.web3Provider = web3.currentProvider
+            window.web3 = new Web3(web3.currentProvider)
+                // Acccounts always exposed
+            web3.eth.sendTransaction({ /* ... */ })
+        }
+        // Non-dapp browsers...
+        else {
+            console.log('Non-Ethereum browser detected.')
+        }
+    },
 
-  loadAccount: async () => {
-    // Set the current blockchain account
-    App.account = web3.eth.accounts[0]
-  },
+    loadAccount: async() => {
+        // Set the current blockchain account
+        App.account = web3.eth.accounts[0]
+    },
 
-  loadContract: async () => {
-    // Create a JavaScript version of the smart contract
-    const  PlatformContract = await $.getJSON('PlatformContract.json')
-    App.contracts.Platform = TruffleContract(PlatformContract)
-    App.contracts.Platform.setProvider(App.web3Provider)
+    loadContract: async() => {
+        // Create a JavaScript version of the smart contract
+        const PlatformContract = await $.getJSON('PlatformContract.json')
+        App.contracts.Platform = TruffleContract(PlatformContract)
+        App.contracts.Platform.setProvider(App.web3Provider)
 
-    // Hydrate the smart contract with values from the blockchain
-    App.Platform = await App.contracts.Platform.deployed()
-  },
+        // Hydrate the smart contract with values from the blockchain
+        App.Platform = await App.contracts.Platform.deployed()
+    },
 
-  render: async () => {
-    App.loader = $("#loader");
-    App.content = $("#content");
-    
-    App.setLoading(true)
-    
-    // Render Account
-    $('#accountAddress').html(App.account)
-    
-    App.setLoading(false)
-  },
-  
+    render: async() => {
+        App.loader = $("#loader");
+        App.content = $("#content");
 
-  userRecharge: async (count) => {
-    App.Platform.addMoney(count)
-  },
+        App.setLoading(true)
 
-  userPostOffer: async (itemId, tradePrice) => {
-    App.Platform.makeOffer(itemId, tradePrice)
-  },
+        // Render Account
+        $('#accountAddress').html(App.account)
 
-  userBuy: async (itemId) => {
-    App.Platform.butItem(itemId)
-  },
+        App.setLoading(false)
+    },
 
-  userTrade: async (offerId) => {
-    App.Platform.tradeItem(offerId)
-  },
 
-  
-  setLoading: async (boolean) => {
-    if (boolean) {
-      App.loader.show();
-      App.content.hide();
-    } else {
-      App.loader.hide();
-      App.content.show();
-    }
-  },
+    userRecharge: async(count) => {
+        App.Platform.addMoney(count)
+    },
+
+    userPostOffer: async(itemId, tradePrice) => {
+        App.Platform.makeOffer(itemId, tradePrice)
+    },
+
+    userBuy: async(itemId) => {
+        App.Platform.butItem(itemId)
+    },
+
+    userTrade: async(offerId) => {
+        App.Platform.tradeItem(offerId)
+    },
+    userCancelOffer: async(offerId) => {
+        App.Platform.recallItem(offerId)
+    },
+
+
+
+
+    setLoading: async(boolean) => {
+        if (boolean) {
+            App.loader.show();
+            App.content.hide();
+        } else {
+            App.loader.hide();
+            App.content.show();
+        }
+    },
 }
 
 $(() => {
-  $(window).load(() => {
-    App.load()
-  })
+    $(window).load(() => {
+        App.load()
+    })
 })
